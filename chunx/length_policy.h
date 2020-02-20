1c8aaf6149af15b61b2551d88e1d3773911ee0db
markovchainy <markovchainy@users.noreply.github.com>
2020-02-20 00:33:26 +0000

    Consume full input when size is 0 in length policy

--- chunx/length_policy.h
+++ chunx/length_policy.h
@@ -15,15 +15,16 @@ class length_policy : public policy<InputIt, Container> {
     using input_type = typename std::iterator_traits<InputIt>::value_type;
     using view_type = typename std::basic_string_view<input_type>;
     auto input_size = std::size_t(std::distance(first, last));
-    auto chunk_size = std::min(input_size, get_length());
+    auto size = get_length();
+    size = size == 0 ? input_size : std::min(input_size, size);
     if constexpr (std::is_same_v<Container, view_type>) {
-      token = view_type{&*first, chunk_size};
+      token = view_type{&*first, size};
     } else {
-      token = Container{first, std::next(first, chunk_size)};
+      token = Container{first, std::next(first, size)};
     }
-    std::advance(first, chunk_size);
-    return chunk_size > 0;
+    std::advance(first, size);
+    return size > 0;
   }
-};
+};  // namespace chunx
 
 }  // namespace chunx
